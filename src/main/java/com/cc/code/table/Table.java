426b64a1398a43a1325a547c77620b8079bfd874
lilaizhen <lilaizhencn@gmail.com>
2018-03-28 16:01:47 +0800

    优化 精简。

--- src/main/java/com/cc/code/table/Table.java
+++ src/main/java/com/cc/code/table/Table.java
@@ -1,5 +1,7 @@
 package com.cc.code.table;
 
+import lombok.Data;
+
 import java.util.List;
 import java.util.Set;
 
@@ -8,53 +10,84 @@ import java.util.Set;
  * @author bigcong
  *
  */
-@SuppressWarnings("all")
+@Data
 public class Table {
-    private String className;// 原表名称
-
-    private String className_d;// 大写表名称
-
-    private String className_x;// 小写表名称
-
+    /**
+     * 原表名称
+     */
+    private String className;
+    /**
+     * 大写表名称
+     */
+    private String className_d;
+    /**
+     * 小写表名称
+     */
+    private String className_x;
+    /**
+     *
+     */
     private String packageName;
-
-    private List<TableCarray> tableCarrays;// 表字段
-
+    /**
+     * 表字段
+     */
+    private List<TableCarray> tableCarrays;
+    /**
+     *
+     */
     private String key;
-
-    private String key_x;// 主键小写
-
-    private String key_d;// 主键大写
-
-    public String getKey_d() {
-        return key_d;
-    }
-
-    public void setKey_d(String key_d) {
-        this.key_d = key_d;
-    }
-
-    private List<TableIndex> tableIndexs;// 表索引
-
-    private List<TableBind> tableBinds;// 表主外键
-
-    private Set<String> importPojos;// 需要导入的POJO
-
-    private String stringCarrayNames1;// ","拼接大写字段
-
-    private String stringCarrayNames2;// int id ,String userCord ,..
-
-    private String stringCarrayNames3;// ","拼接原字段
-
-    private String stringCarrayNames4;// "#%s#,"拼接小写字段
-
-    private String stringCarrayNames5;// "%s=#%s#,"拼接原字段-小写字段
-
-    private String stringCarrayNames6;// "%s=#%s#,"拼接原字段-小写字段
-
-    private String stringCarrayNames7;// "%s=#%s#,"拼接原字段-小写字段
-
-    private String stringCarrayNames8;//根据主键得到实体
+    /**
+     * 主键小写
+     */
+    private String key_x;
+    /**
+     * 主键大写
+     */
+    private String key_d;
+    /**
+     * 表索引
+     */
+    private List<TableIndex> tableIndexs;
+    /**
+     * 表主外键
+     */
+    private List<TableBind> tableBinds;
+    /**
+     * 需要导入的POJO
+     */
+    private Set<String> importPojos;
+    /**
+     * ","拼接大写字段
+     */
+    private String stringCarrayNames1;
+    /**
+     * int id ,String userCord ,..
+     */
+    private String stringCarrayNames2;
+    /**
+     * // ","拼接原字段
+     */
+    private String stringCarrayNames3;
+    /**
+     * // "#%s#,"拼接小写字段
+     */
+    private String stringCarrayNames4;
+    /**
+     * // "%s=#%s#,"拼接原字段-小写字段
+     */
+    private String stringCarrayNames5;
+    /**
+     * // "%s=#%s#,"拼接原字段-小写字段
+     */
+    private String stringCarrayNames6;
+    /**
+     * // "%s=#%s#,"拼接原字段-小写字段
+     */
+    private String stringCarrayNames7;
+    /**
+     * //根据主键得到实体
+     */
+    private String stringCarrayNames8;
 
     public Table(String className, String classNameD, String classNameX, String packageName,
             List<TableCarray> tableCarrays, List<TableIndex> tableIndexs, List<TableBind> tableBinds,
@@ -78,149 +111,4 @@ public class Table {
         this.stringCarrayNames6 = stringCarrayNames6;
         this.stringCarrayNames7 = stringCarrayNames7;
     }
-
-    public String getKey() {
-        return key;
-    }
-
-    public void setKey(String key) {
-        this.key = key;
-    }
-
-    public String getStringCarrayNames8() {
-        return stringCarrayNames8;
-    }
-
-    public void setStringCarrayNames8(String stringCarrayNames8) {
-        this.stringCarrayNames8 = stringCarrayNames8;
-    }
-
-    public String getClassName() {
-        return className;
-    }
-
-    public String getKey_x() {
-        return key_x;
-    }
-
-    public void setKey_x(String key_x) {
-        this.key_x = key_x;
-    }
-
-    public void setClassName(String className) {
-        this.className = className;
-    }
-
-    public String getClassName_d() {
-        return className_d;
-    }
-
-    public void setClassName_d(String classNameD) {
-        className_d = classNameD;
-    }
-
-    public String getClassName_x() {
-        return className_x;
-    }
-
-    public void setClassName_x(String classNameX) {
-        className_x = classNameX;
-    }
-
-    public List<TableCarray> getTableCarrays() {
-        return tableCarrays;
-    }
-
-    public void setTableCarrays(List<TableCarray> tableCarrays) {
-        this.tableCarrays = tableCarrays;
-    }
-
-    public List<TableIndex> getTableIndexs() {
-        return tableIndexs;
-    }
-
-    public void setTableIndexs(List<TableIndex> tableIndexs) {
-        this.tableIndexs = tableIndexs;
-    }
-
-    public List<TableBind> getTableBinds() {
-        return tableBinds;
-    }
-
-    public void setTableBinds(List<TableBind> tableBinds) {
-        this.tableBinds = tableBinds;
-    }
-
-    public Set<String> getImportPojos() {
-        return importPojos;
-    }
-
-    public void setImportPojos(Set<String> importPojos) {
-        this.importPojos = importPojos;
-    }
-
-    public String getStringCarrayNames1() {
-        return stringCarrayNames1;
-    }
-
-    public void setStringCarrayNames1(String stringCarrayNames1) {
-        this.stringCarrayNames1 = stringCarrayNames1;
-    }
-
-    public String getStringCarrayNames2() {
-        return stringCarrayNames2;
-    }
-
-    public void setStringCarrayNames2(String stringCarrayNames2) {
-        this.stringCarrayNames2 = stringCarrayNames2;
-    }
-
-    public String getStringCarrayNames3() {
-        return stringCarrayNames3;
-    }
-
-    public void setStringCarrayNames3(String stringCarrayNames3) {
-        this.stringCarrayNames3 = stringCarrayNames3;
-    }
-
-    public String getStringCarrayNames4() {
-        return stringCarrayNames4;
-    }
-
-    public void setStringCarrayNames4(String stringCarrayNames4) {
-        this.stringCarrayNames4 = stringCarrayNames4;
-    }
-
-    public String getStringCarrayNames5() {
-        return stringCarrayNames5;
-    }
-
-    public void setStringCarrayNames5(String stringCarrayNames5) {
-        this.stringCarrayNames5 = stringCarrayNames5;
-    }
-
-    public String getStringCarrayNames6() {
-        return stringCarrayNames6;
-    }
-
-    public void setStringCarrayNames6(String stringCarrayNames6) {
-        this.stringCarrayNames6 = stringCarrayNames6;
-    }
-
-    public String getStringCarrayNames7() {
-        return stringCarrayNames7;
-    }
-
-    public void setStringCarrayNames7(String stringCarrayNames7) {
-        this.stringCarrayNames7 = stringCarrayNames7;
-    }
-
-    public String getPackageName() {
-        return packageName;
-    }
-
-    public void setPackageName(String packageName) {
-        this.packageName = packageName;
-    }
-
 }
